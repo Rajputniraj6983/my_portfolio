026e0d2fbbbbaa5f608dc2d87a51344bfb40bde2
NIRAJ <nirarajput6983@gmail.com>
2024-05-18 20:48:02 +0530

    18-05-2024

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_portpolio/Constant.dart';
+import 'package:my_portpolio/components/area_info_text.dart';
+import 'package:my_portpolio/components/side_menu.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -14,33 +16,46 @@ class MainScreen extends StatelessWidget {
             children: [
               Expanded(
                 flex: 2,
-                child: Drawer(
-                  child: Column(
-                    children: [
-                      AspectRatio(
-                        aspectRatio: 1.23,
-                        child: Container(
-                          color: Color(0xFF242430),
-                          child: Column(
-                            children: [
-                              CircleAvatar(radius: 50,backgroundImage: AssetImage("assets/images.png "),
-                              ),
-                              Text('niraj rajput',style: Theme.of(context).textTheme.subtitle2,)
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
+                child: SideMenu(),
               ),
               Expanded(
                 flex: 7,
-                child: Container(color: Colors.blue),
+                child: Container(color: Colors.black),
               )
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Drawer slidermenu(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          AspectRatio(
+            aspectRatio: 1.23,
+            child: Container(
+              color: Color(0xFF242430),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Spacer(
+                    flex: 2,
+                  ),
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage("assets/images.png "),
+                  ),
+                  Text(
+                    'Niraj rajput',
+                    style: Theme.of(context).textTheme.subtitle2,
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
---
+++ lib/projects.dart
@@ -0,0 +1,28 @@
+class Project {
+  final String? title, description;
+
+  Project({this.title, this.description});
+}
+
+List <Project> myProjects = [
+  Project(
+      title: "Quotify - A Quote App",
+      description: "Quotify: A straightforward Flutter application that displays inspirational quotes"
+  ),
+  Project(
+      title: "",
+      description: ""
+  ),
+  Project(
+      title: "",
+      description: ""
+  ),
+  Project(
+      title: "",
+      description: ""
+  ),
+  Project(
+      title: "",
+      description: ""
+  ),
+];
